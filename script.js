050835d15cc34332ecc739a5a43237b3e2f2d3fa
new-biee <anhvtth1907004@fpt.edu.vn>
2020-10-26 15:21:59 +0700

    Add file

--- script.js
+++ script.js
@@ -2,34 +2,101 @@
  * Created by nhatnk on 4/26/17.
  */
 
-function Hero(image, top, left, size){
-  this.image = image;
-  this.top = top;
-  this.left = left;
-  this.size = size;
+class Hero {
+    constructor(image, top, left, size, width, height, speed, direction) {
+        this.image = image;
+        this.top = top;
+        this.left = left;
+        this.size = size;
+        this.width = width;
+        this.height = height;
+        this.speed = speed;
+        this.direction = direction;
+    }
 
-  this.getHeroElement = function(){
-    return '<img width="'+ this.size + '"' +
-      ' height="'+ this.size + '"' +
-      ' src="' + this.image +'"' +
-      ' style="top: '+this.top+'px; left:'+this.left+'px;position:absolute;" />';
-  }
+    getHero() {
+        return `<img src="${this.image}" alt="Hero" width="${this.width}px" height= ${this.height}px style="top:${this.top}px; left:${this.left}px; position:absolute;">`;
+    }
 
-  this.speed = function(){
-    this.left += 10;
-    console.log('ok: ' + this.left);
-  }
+    moveLeft() {
+        this.left -= this.speed;
+    }
 
+    moveRight() {
+        this.left += this.speed;
+    }
+
+    moveTop() {
+        this.top -= this.speed;
+    }
+
+    moveBottom(){
+        this.top +=this.speed
+    }
+
+    changeDirection(direction) {
+        this.direction = direction;
+    }
+
+    draw() {
+        document.getElementById("game").innerHTML = this.getHero();
+    }
 }
 
-var hero = new Hero('car.jpg', 10, 10, 100);
 
-function start(){
-  if(hero.left < window.innerWidth - hero.size){
-    hero.speed();
-  }
-  document.getElementById('game').innerHTML = hero.getHeroElement();
-  setTimeout(start, 500)
-}
+const LEFT = 37;
+const RIGHT = 39;
+const TOP = 38;
+const BOTTOM = 40;
+const STOP = 32;
+let action = [LEFT, RIGHT, TOP, BOTTOM, STOP];
 
-start();
+let hero = new Hero('car.jpg',20, 0, 100, 110, 50, 1, RIGHT);
+
+const move = direction => {
+    switch (direction) {
+        case LEFT:
+            hero.moveLeft();
+            break;
+        case RIGHT:
+            hero.moveRight();
+            break;
+        case TOP:
+            hero.moveTop();
+            break;
+        case BOTTOM:
+            hero.moveBottom();
+            break;
+        case STOP:
+            hero.stop();
+            break;
+    }
+};
+
+const run = function (){
+    idAnimation = requestAnimationFrame(run);
+    hero.draw();
+    move(hero.direction);
+};
+
+const start = keyCode => {
+    requestAnimationFrame(run);
+};
+
+const stop = keyCode => {
+    requestAnimationFrame(idAnimation);
+};
+
+let idAnimation = requestAnimationFrame(run);
+
+document.addEventListener("keydown", e => {
+    let isKeyCode = action.indexOf(e.keyCode);
+    if (isKeyCode !== -1) {
+        if (hero.direction === STOP && e.keyCode > STOP) {
+            start();
+        } else if (e.keyCode === STOP) {
+            stop();
+        }
+        hero.changeDirection(e.keyCode);
+    }
+});
